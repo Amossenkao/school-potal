6238c1772517046716d8e6112c41996a6aa72f15
= <senkao.a@outlook.com>
2026-02-02 02:50:08 +0000

    Optimized yearly and periodic reports for mobile

--- app/dashboard/layout/AppHeader.tsx
+++ app/dashboard/layout/AppHeader.tsx
@@ -329,7 +329,7 @@ const NotificationsDropdown = () => {
 			</button>
 
 			{isOpen && (
-				<div className="absolute right-0 mt-2 w-72 max-w-[90vw] sm:w-80 origin-top-right rounded-xl bg-white dark:bg-gray-800 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none z-50">
+				<div className="absolute right-0 mt-2 w-72 max-w-[85vw] sm:w-80 origin-top-right rounded-xl bg-white dark:bg-gray-800 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none z-50 -translate-x-2 sm:translate-x-0">
 					<div className="p-4 border-b border-gray-200 dark:border-gray-700">
 						<div className="flex items-center justify-between">
 							<h3 className="text-lg font-semibold text-gray-800 dark:text-white">
@@ -582,27 +582,8 @@ const AppHeader: React.FC = () => {
 					<div className="ml-auto flex items-center gap-2 lg:hidden">
 						<NotificationsDropdown />
 						<ThemeToggleButton />
+						<UserDropdown />
 					</div>
-
-					<button
-						onClick={() => setApplicationMenuOpen(!isApplicationMenuOpen)}
-						className="flex items-center justify-center w-10 h-10 text-gray-700 rounded-lg z-40 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-800 lg:hidden"
-					>
-						<svg
-							width="24"
-							height="24"
-							viewBox="0 0 24 24"
-							fill="none"
-							xmlns="http://www.w3.org/2000/svg"
-						>
-							<path
-								fillRule="evenodd"
-								clipRule="evenodd"
-								d="M5.99902 10.4951C6.82745 10.4951 7.49902 11.1667 7.49902 11.9951V12.0051C7.49902 12.8335 6.82745 13.5051 5.99902 13.5051C5.1706 13.5051 4.49902 12.8335 4.49902 12.0051V11.9951C4.49902 11.1667 5.1706 10.4951 5.99902 10.4951ZM17.999 10.4951C18.8275 10.4951 19.499 11.1667 19.499 11.9951V12.0051C19.499 12.8335 18.8275 13.5051 17.999 13.5051C17.1706 13.5051 16.499 12.8335 16.499 12.0051V11.9951C16.499 11.1667 17.1706 10.4951 17.999 10.4951ZM13.499 11.9951C13.499 11.1667 12.8275 10.4951 11.999 10.4951C11.1706 10.4951 10.499 11.1667 10.499 11.9951V12.0051C10.499 12.8335 11.1706 13.5051 11.999 13.5051C12.8275 13.5051 13.499 12.8335 13.499 12.0051V11.9951Z"
-								fill="currentColor"
-							/>
-						</svg>
-					</button>
 				</div>
 				<div
 					className={`${
@@ -613,7 +594,9 @@ const AppHeader: React.FC = () => {
 						<NotificationsDropdown />
 						<ThemeToggleButton />
 					</div>
-					<UserDropdown />
+					<div className="hidden lg:block">
+						<UserDropdown />
+					</div>
 				</div>
 			</div>
 		</header>
