2b1f971a469baedf262f7303ce6556b20b008afd
Amos Senkao <senkao.a@outlook.com>
2026-02-09 15:34:28 +0000

    Added PWA support

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,6 +23,9 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="en">
+			<head>
+				<link rel="manifest" href="/manifest.webmanifest" />
+			</head>
 			<body>
 				<RootProviders>{children}</RootProviders>
 			</body>
